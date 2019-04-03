8a6c821be0e116fc5c57949532a917651eb23f89
Lê Đức Thiện <leducthien.bk@gmail.com>
2019-04-03 15:38:56 +0700

    Complete ToS updated

--- protected/components/form/views/login_form.php
+++ protected/components/form/views/login_form.php
@@ -33,8 +33,7 @@
 	</div>
 	<hr>
 	<div class="form-group" type="button">
-		<a id="myBtn"> ads</a> 
-		<a href="<?php echo $this->createUrl("/site/login_with_facebook") ?>" class="btn fb-button"><?php $form->l_("Đăng nhập với Facebook") ?></a>
+		<button id="myBtn" class="fb-button" type="button"> Đăng nhập với Facebook </button> 
 	</div>
 	<div id="myModal" class="modal">
 		<!-- Modal content -->
@@ -44,7 +43,9 @@
 				<h2 style="font-size:20px;color:white;">Điều khoản sử dụng</h2>
 			</div>
 
-			<div id="term-of-use" class="modal-body" onscroll='checkBottom()' style="color:black">
+			<div id="term-of-use" class="modal-body" style="color:black">
+
+				<p class="headline">QUY ĐỊNH KÝ GỬI</p>
 
 				<p class="title">1.1   Đối với hàng ký gửi chúng tôi chỉ nhận hàng về kho Bằng Tường.</p>
 
@@ -112,24 +113,138 @@
 
 				<p>Nếu khách hàng vẫn muốn lấy hàng khi công ty đã có lời hẹn lại thì công ty chịu trách nhiệm đền bù số tiền bằng 5% giá trị tiền hàng của số hàng đó.</p>
 
+				<br> <br>
+
+				<p class="headline">QUY ĐỊNH GIAO NHẬN HÀNG </p>
+
+				<p>· Giao nhận hàng là công đoạn cuối cùng của quá trình Order. Để việc giao nhận được thuận tiện và không sảy ra mất mát hàng hóa hoặc nhầm lẫn thì OrderHip có một số quy định như sau:</p>
+
+				<p>- Để đáp ứng nhu cầu của khách hàng chúng tôi có thể giao hàng tại kho hoặc giao hàng tại nơi khách hàng yêu cầu:</p>
+
+				<p class="title">1.1 Giao hàng tại kho:</p>
+
+				<p>- Quý khách hoặc người được ủy quyền phải đến kho của chúng tôi kiểm và nhận hàng theo mã đơn hàng trên Web. Khi quý khách đã kiểm hàng và đồng ý nhận hàng thì yêu cầu quý khách thanh lý đơn hàng trước khi mang hàng về. Nếu hàng hóa sau khi mang ra khỏi kho OrderHip không chịu trách nhiệm về mọi khiếu nại. </p>
+
+				<p class="title">1.2 Giao hàng hoặc gửi hàng đi cho khách:</p>
+
+				<p>- Orderhip hỗ trợ đi gửi hàng hoặc giao hàng đến tận nhà cho quý khách, tuy nhiên khách hàng phải chịu chi phí gửi hàng từ kho OrderHip đến địa điểm quý khách yêu cầu, vì là quý khách không nhận hàng tại kho nên việc kiểm hàng sẽ diễn ra tại địa điểm quý khách nhận hàng.</p>
+
+				<p class="title">1.3 Hàng bị lỗi hoặc thiếu cần phải báo ngay lập tức.</p>
+
+				<p class="title">- Vì là công ty trung gian giao dịch giữa người bán và người mua nên có những vấn đề chúng tôi không thể hoàn toàn chủ động được. Do vậy nên khi khách hàng đã nhận được hàng mà có bất kì sự cố gì thì yêu cầu khách hàng báo ngay lại cho công ty. Sau hai ngày chúng tôi sẽ không hỗ trợ giải quyết những vấn đề đó. </p>
+
+				<p class="title">1.4 Không lưu hàng tại kho quá 2 ngày:</p>
+
+				<p>- Khi chúng tôi đã có thông báo hàng về kho VN thì quý khách vui lòng đến kho nhận hàng hoặc yêu cầu công ty hỗ trợ gửi hàng. Nếu để lưu kho quá hai ngày công ty sẽ tính phí lưu kho 0.5% giá trị đơn hàng/ngày.</p>
+
+				<p class="title">1.5 Thanh lý tiền đơn hàng trước khi chuyển hàng đi.</p>
+
+				<p>- Công ty chỉ chuyển hàng đi khi có xác nhận của kế toán về việc khách hàng đã thanh lý xong hết tiền đơn hàng.</p>
+
+				<p class="title">1.6 Công ty chỉ giao hàng đến địa chỉ khách hàng yêu cầu, không bao gồm bốc xếp, hạ hàng từ xe xuống.</p>
+
+				<p>- Công ty chỉ có trách nhiệm mang hàng đến địa chỉ của khách hàng khi có sự yêu cầu từ quý khách. Ngoài ra công ty sẽ không chịu trách nhiệm về những phát sinh thêm khi không có sự yêu cầu từ quý khách ( bốc xếp hàng hóa tại địa điểm giao hàng, lưu giữ xe hàng, nhân công quá lâu tại địa điểm bàn giao hàng…)</p>
+
+				<p class="title">1.7 Giải quyết khiếu nại</p>
+
+				<p class="subTitle">1.7.1 Thời gian kể từ khi khách hàng có yêu cầu chuyển hàng đến khi hàng bắt đầu được chuyển đi trong khoảng hai ngày.</p>
+
+				<p>- Sau khi khách hàng đã thanh lý đơn hàng và có yêu cầu chuyển hàng đến địa chỉ khách hàng yêu cầu chúng tôi sẽ tiếp nhận và xử lý nhanh nhất có thể. Tuy nhiên, việc sắp xếp chuyển giao có thể lên tới hai ngày. Nếu trường hợp hai ngày mà công ty vẫn chưa chuyển hàng cho quý khách thì kể từ ngày thứ ba trở ra công ty chịu trách nhiệm với những thiệt hại về kinh tế mà quý khách phải chịu.</p>
+
+				<p class="subTitle">1.7.2 Giao hàng bị thiếu, hỏng, mất hoặc bị trầy xước.</p>
+
+				<p>- Công ty sẽ đền bù đúng giá trị tương đương với giá trị hàng hóa mất đi hoặc giá trị cần thiết để phục hồi sửa chữa. Trường hợp trầy xước trong phạm vi cho phép do quá trình vận chuyển không thể tránh khỏi thì mong khách hàng thông cảm và tự khắc phục. Việc đánh giá xem xét mức độ thiệt hại về hàng hóa sẽ phải được cả hai bên đánh giá và  thống nhất.</p>
+
+				<br> <br>
+
+				<p class="headline">QUY ĐỊNH GIAO NHẬN HÀNG </p>
+
+				<p class="title">1.1   Hai hình thức thanh toán là giao tiền mặt và chuyển khoản
+				</p>
+				<p>- Giao tiền mặt: Khách hàng trực tiếp đến văn phòng giao tiền cho kế toán, trường hợp khách hàng giao tiền mặt cho nhân viên không tại văn phòng cần liên hệ với kế toán để xác minh. Nếu không có sự xác minh từ kế toán của công ty thì công ty sẽ không chịu trách nhiệm trước số tiền quý khách đã bàn giao.
+				</p>
+				<p>- Chuyển khoản qua số tài khoản ngân hàng: Chỉ được phép chuyển qua các số tài khoản để trên web, ngoài ra công ty cũng sẽ không chịu trách nhiệm giải quyết.
+				</p>
+				<p class="title">1.2 Khi chuyển tiền cần ghi rõ nội dung thanh toán.
+				</p>
+				<p>- Quý khách cần ghi rõ nội dung chuyển khoản: nếu đặt cọc quý khách viết với nội dung: ĐẶT CỌC [mã đơn], thanh lý: THANH LÝ [mã đơn] ngoài ra chúng tôi cũng không thể chịu trách nhiệm nếu khoản tiền không có nội dung. 
+				</p>
+				<p class="title">1.3 Việc hồi tiền hoặc bồi thường tiền cho quý khách có thể giải quyết trong ba ngày.
+				</p>
+				<p>- Mọi chi phí thanh toán ngược lại cho khách hàng cần được kiểm tra, giải quyết nên thời gian trả lại tiền cho quý khách có thể mất ba ngày. Mong quý khách thông cảm!
+
+				<br> <br>
+
+				<p class="headline"> QUY ĐỊNH ORDER </p>
+
+				<p class="title">1.1 OrderHip không nhận order hàng hóa mà nhà nước Việt Nam cấm trao đổi mua bán và vận chuyển.
+				</p>
+				<p>- Một số mặt hàng theo quy định nhà nước cấm vận chuyển và trao đổi mua bán: Vũ khí, pháo hoa, trang thiết bị quân sự, chất ma túy, hóa chất, thuốc thú ý, phân bón, động thực vật....
+				</p>
+				<p class="title">1.2 OrderHip không chịu trách nhiệm về chất lượng và hình dáng hàng hóa nếu như trong quá trình giao dịch không có sự cam kết giữa hai bên.
+				</p>
+				<p>- Trước khi đặt hàng khách hàng cần nhờ nhân viên mình trực tiếp làm việc để hỏi những vấn đề chưa nắm rõ về sản phẩm, trong quá trình giao dịch khách cần lưu lại những mục đã thỏa thuận giữa hai bên. Trường hợp hàng về có gặp vấn đề gì hai bên có căn cứ giải quyết. Những thông tin mà hai bên không thỏa thuận chúng tôi sẽ không chịu trách nhiệm khi hàng về sai.
+				</p>
+				<p class="title">1.3 Không chịu trách nhiệm đối với hàng hóa dễ vỡ, hoặc dễ bóp méo trong quá trình vận chuyển nếu như khách hàng không yêu cầu đóng kiện gỗ.
+				</p>
+				<p>- Vì đặc thù vận chuyển xa, nâng hạ nhiều lần, và gom nhiều sản phẩm trên xe hàng vì vậy nên những hàng hóa dễ vỡ khách hàng cần yêu cầu nhân viên hỗ trợ giao dịch với xưởng đóng kiện gỗ hoặc nhờ Orderhip đóng kiện lại tại kho của Orderhip.
+				</p>
+				<p class="title">1.4 Không thể cam kết chắc chắn ngày hàng về Việt Nam.
+				</p>
+				<p>- Trong quá trình mua hàng, hàng hóa có thể về nhanh hoặc chậm hơn so với mức áng chừng nhân viên đưa ra, bởi có thể do xưởng shop phát hàng chậm hoặc ở xa, thậm trí trong quá trình vận chuyển có thể gặp rủi ro. Nếu hàng về chậm quá 10 ngày so với ngày hẹn quý khách có thể lấy lại tiền hàng. Chúng tôi cập nhật tình trạng hàng hóa trên web: orderhip.com				
+				</p>
+				<p class="title">1.5 GIẢI QUYẾT KHIẾU NẠI
+				</p>
+				<p>· Lưu ý: OH chỉ nhận khiếu nại khi hàng đến tay khách hàng không quá 1 ngày, Quá thời gian trên OH sẽ không chấp nhận khiếu nại do không biết nguyên nhân do bên nào nên mong quý khách kiểm tra hàng kỹ trước khi nhận.
+				</p>
+				<p>- Thời gian giải quyết khiếu nại từ 1-3 ngày kể từ ngày khách hàng khiếu nại.
+				</p>
+				<p class="subTitle">1.5.1 Đối với trường hợp mất hoặc thiếu hàng: 
+				</p>
+				<p>- Đền bù 100% giá trị tiền hàng bị thiếu hoặc mất.
+				</p>
+				<p class="subTitle">1.5.2 Đối với trường hợp hàng hóa bị xây xước, bóp méo trong quá trình vận chuyển từ TQ về VN:
+				</p>
+				<p>- Đền bù từ 10% - 30% giá trị tiền hàng tùy theo mức độ (thỏa thuận).
+				</p>
+				<p class="subTitle">1.5.3 Đối với trường hợp hàng hóa bị hỏng hoàn toàn do quá trình vận chuyển từ TQ về VN: 
+				</p>
+				<p>- TH1: Đổi mới lại cho quý khách và chịu mọi chi phí đổi trả.
+				</p>
+				<p>- TH2: Đền bù 100% giá trị tiền hàng
+				</p>
+				<p class="subTitle">1.5.4 Hàng hóa về bị sai so với thông tin đã thỏa thuận, lỗi do công ty cam kết sai
+				</p>
+				<p>- TH1: Trường hợp đổi trả: Chịu trách nhiệm 100% chi phí vận chuyển lại cho người bán.
+				</p>
+				<p>- TH2: Trường hợp không thể trả lại: Chịu trách nhiệm đền bù cho khách hàng khoản thiệt hại về hàng hóa mà khách hàng phải chịu, Việc xác định thiệt hại hai bên đều phải đánh giá và đi đến thống nhất dựa trên việc thỏa thuận.
+				</p>
+				<p class="subTitle">1.5.5 Hàng Hóa về bị sai thông tin do lỗi của khách hàng hoặc không có trong thỏa thuận của hai bên.
+				</p>
+				<p>- Hỗ trợ khách hàng đàm phán với người bán giải quyết khiếu nại
+				</p>
+				<p>- Hỗ trợ khách hàng vận chuyển nếu xảy ra việc đổi trả. Chi phí vận chuyển khách hàng chịu 100%.
+				</p>
+				<p>- Trường hợp đền bù bằng giá trị tiền thì công ty sẽ trả lại khách hàng đúng giá trị tiền mà người bán đền bù sau quá trình thỏa thuận và đi đến thống nhất.
+				</p>
+				<p class="subTitle">1.5.6 Hàng hóa bị thất lạc hoặc không có thông tin
+				</p>
+				<p>- Hàng về muộn sau 10 ngày kể từ ngày hẹn hàng về VN khách hàng có quyền không nhận hàng và lấy lại 100% số tiền đã đặt cọc.
+				</p>
+				<p>- Nếu khách hàng vẫn muốn lấy hàng khi công ty đã có lời hẹn lại thì công ty chịu trách nhiệm đền bù số tiền bằng số tiền phí dịch vụ mà khách hàng đã trả cho đơn hàng đó.
+				</p>
 
 			</div>
 
 			<div class="modal-footer">
 				<!-- Disable the accept button by default -->
-				<button id="closeButton" class="btn btn-danger"> Decline </button>
-				<button id="accept-button" class="btn btn-info" disabled> Accept </button>
+				<button id="closeButton" class="btn btn-danger" type="button"> Từ chối </button>
+				<!-- <button id="accept-button" class="btn btn-info" type="button" disabled> Accept </button> -->
+				<a id="accept-button" href="<?php echo $this->createUrl("/site/login_with_facebook") ?>" class="btn btn-info"><?php $form->l_("Đồng ý") ?></a>
 			</div>
 		</div>
 	</div>
 <?php $form->close(); ?>
-<script>
-	$(function(){
-		$("#login-form").on("form-success",function(){
-			location.href = "<?php echo LoginHelper::getLoginCallbackUrl() ?>";
-		});
-	});
-</script>
 
 <style>
 .textbox {
@@ -237,10 +352,12 @@
 /* ToS styling - thienld 3/4*/
 .title {
 	font-weight: bold;
+	font-size: 115%;
 }
 
 .subTitle {
 	font-style: italic;
+	font-size: 110%;
 }
 
 .headline {
@@ -250,6 +367,13 @@
 </style>
 
 <script>
+	//Login script
+	$(function(){
+			$("#login-form").on("form-success",function(){
+				location.href = "<?php echo LoginHelper::getLoginCallbackUrl() ?>";
+			});
+		});
+
 	// Get the modal
 	var modal = document.getElementById('myModal');
 
@@ -284,11 +408,4 @@
 		if (event.target == modal) {
 			modal.style.display = "none";
 	}}
-
-	// When user scrolled the entire term of use, enable the accept button
-	function checkBottom(){
-		if ((textElement.scrollTop + textElement.offsetHeight) >= textElement.scrollHeight){
-			document.getElementById("accept-button").disabled = false;
-		}
-	}
 </script>

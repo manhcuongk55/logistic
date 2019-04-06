48741d21f1032c8b2cda45580f7d5a6be04283a5
hb162 <hoangbui1699@gmail.com>
2019-04-06 15:48:18 +0700

    dummy commit

--- protected/config/db.php
+++ protected/config/db.php
@@ -1,9 +1,9 @@
 <?php
 return array(
-	'connectionString' => 'mysql:host=127.0.0.1;dbname=orderhip', 
+	'connectionString' =>'mysql:host=127.0.0.1;dbname=orderhip', 
 	'emulatePrepare' => true, 
 	'username' => 'root', 
 	'password' => 'Welcome1', 
 	'charset' => 'utf8', 
-	"tablePrefix" => "tbl_"
+	"tablePrefix" => "tbl_" 
 );
